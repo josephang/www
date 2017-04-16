9ade3361d73e36f336826fa389d26a880ebf0d87
josephang <josephang05@gmail.com>
2017-04-16 01:10:24 -0700

    April Commit

---
+++ cocrc.org/wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://josephang.net/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Created and Maintained by %s', 'twentyseventeen' ), 'Joseph Ang' ); ?></a>
+</div><!-- .site-info -->
